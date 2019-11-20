73ba76963272ccc6a1876700a0040f9a744fd619
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-11-20 18:00:52 +0100

    Update and rename shit.py to Euclidean Algorithm.py

--- Euclidean Algorithm.py
+++ Euclidean Algorithm.py
@@ -10,6 +10,4 @@ def euclidian_alg(number_1, number_2):
         else:
             print(number_1, "=",number_2,"*", math.floor(number_1 / number_2), "+", number_1 % number_2)
 
-
-
 euclidian_alg(23023, 18291)
