3d22c9fd9e3432d11013d4e25c961357948632e8
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-11-20 21:02:05 +0100

    Update Euclidean Algorithm.py

--- Euclidean Algorithm.py
+++ Euclidean Algorithm.py
@@ -1,13 +1,11 @@
 import math
 
 def euclidian_alg(number_1, number_2):
-    print(number_1, "=",number_2,"*", math.floor(number_1 / number_2), "+", number_1 % number_2)
     while True:
+        print(number_1, "=", number_2, "*", math.floor(number_1 / number_2), "+", number_1 % number_2)
         number_1, number_2 = number_2, (number_1 % number_2)
         if number_2 == 0:
             print(number_1)
             return None
-        else:
-            print(number_1, "=",number_2,"*", math.floor(number_1 / number_2), "+", number_1 % number_2)
 
 euclidian_alg(23023, 18291)
